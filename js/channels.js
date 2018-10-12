68d5a9c941b2c11ce74fc54bdf72f4668b2f2d75
moonie68 <43092456+moonie68@users.noreply.github.com>
2018-10-12 17:29:24 -0600

    Update channels.js

--- js/channels.js
+++ js/channels.js
@@ -1,54 +1,60 @@
+/** #7 #chlob #new: the new yummy channel */
 
 var yummy = {
     name: "#Yummy",
-    createdOn: new Date(2016, 03, 01), /* month 0 is jan. */
+    createdOn: new Date(2016, 3, 1), /* month 0 is jan. */
     createdBy: "minus.plus.yummy",
     starred: false,
     expiresIn: 100,
     messageCount: 999,
-    messages : []           // #10 #message s property in #chl object
+    messages: []
 };
+
+/** #7 #chlob #fve: the other channels */
 
 var sevencontinents = {
     name: "#SevenContinents",
-    createdOn: new Date(2016, 03, 02), /* month 0 is jan. */
+    createdOn: new Date(2016, 3, 2), /* month 0 is jan. */
     createdBy: "cheeses.yard.applies",
     starred: true,
     expiresIn: 60,
     messageCount: 5,
-    messages : []           // #10 #message s property in #chl object
-
+    messages: []
 };
 
 var killerapp = {
     name: "#KillerApp",
-    createdOn: new Date(2016, 08, 30), /* month 0 is jan. */
+    createdOn: new Date(2016, 8, 30), /* month 0 is jan. */
     createdBy: "lodge.bits.fake",
     starred: false,
     expiresIn: 1,
     messageCount: 10351,
-    messages : []           // #10 #message s property in #chl object
-
+    messages: []
 };
 
 var firstpersononmars = {
     name: "#FirstPersonOnMars",
-    createdOn: new Date(2016, 08, 28), /* month 0 is jan. */
+    createdOn: new Date(2016, 8, 28), /* month 0 is jan. */
     createdBy: "snipped.atom.grid",
     starred: true,
     expiresIn: 30003,
     messageCount: 2424,
-    messages : []           // #10 #message s property in #chl object
-
+    messages: []
 };
 
 var octoberfest = {
     name: "#Octoberfest",
-    createdOn: new Date(2016, 08, 25), /* month 0 is jan. */
+    createdOn: new Date(2016, 8, 25), /* month 0 is jan. */
     createdBy: "vocally.clearly.crawled",
     starred: false,
     expiresIn: 60,
     messageCount: 321,
-    messages : []           // #10 #message s property in #chl object
-
+    messages: []
 };
+
+
+// create a channel array with the global channels
+var channelArray = [
+    yummy, sevencontinents, killerapp, firstpersononmars, octoberfest
+];
+
